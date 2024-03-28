5723c9267715d1d41716e95408f8f48fd0443ed6
Fabio Pardo <fpardo1023@gmail.com>
2024-03-28 11:42:56 -0400

    update flutter firebase deps and remove items namespace from the project

--- app/main.py
+++ app/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
-from app.routers import items, meals
+from app.routers import meals
 
 app = FastAPI()
 
-app.include_router(items.router)
 app.include_router(meals.router)

1f8ce37d0639fa2d32915f1a3d965125f6c69fae
Fabio Pardo <fpardo1023@gmail.com>
2024-03-29 14:30:48 -0400

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase with the default options
   await Firebase.initializeApp(
     name: 'vida-meals',
     options: DefaultFirebaseOptions.currentPlatform,

af4099159913c8dc2e1a81ba5bad81861edc0deb
Fabio Pardo <fpardo1023@gmail.com>
2024-05-24 18:22:55 -0400

    Add a new meal modal

--- lib/widgets/admin/homepage.dart
+++ lib/widgets/admin/homepage.dart
@@ -10,6 +10,7 @@ class AdminMealsPage extends StatefulWidget {
 
 class _AdminMealsPageState extends State<AdminMealsPage> {
   Set<String> selectedMeals = {};
+
   void toggleSelectedMeals(String mealID) {
     setState(() {
       if (selectedMeals.contains(mealID)) {
@@ -28,10 +29,17 @@ class _AdminMealsPageState extends State<AdminMealsPage> {
       body: FutureBuilder<List<Meal>>(
         future: getMeals(),
         builder: (context, snapshot) {
-          List<Meal>? meals = snapshot.data;
-          if (meals != null && meals.isNotEmpty) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(
+                child: Text('No meals available. Want to create Some?'));
+          } else {
+            List<Meal>? meals = snapshot.data;
             return ListView.builder(
-              itemCount: meals.length,
+              itemCount: meals!.length,
               itemBuilder: (context, index) {
                 Meal meal = meals[index];
                 return ListTile(
@@ -47,9 +55,6 @@ class _AdminMealsPageState extends State<AdminMealsPage> {
                 );
               },
             );
-          } else {
-            return const Center(
-                child: Text('No meals available. Want to create Some?'));
           }
         },
       ),
@@ -62,8 +67,110 @@ class _AdminMealsPageState extends State<AdminMealsPage> {
           child: const Icon(Icons.check),
         ),
       ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ElevatedButton(
+          onPressed: () {
+            showAddMealModal(context, () {
+              setState(() {}); // Trigger a refresh of the FutureBuilder
+            });
+          },
+          child: const Text('Make a new meal'),
+        ),
+      ),
     );
   }
+}
+
+void showAddMealModal(BuildContext context, VoidCallback onAddMeal) {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Add New Meal',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Meal Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a meal name';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Meal Description',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a meal description';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: priceController,
+                decoration: const InputDecoration(
+                  labelText: 'Meal Price',
+                  prefixText: '\$',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a meal price';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Please enter a valid number';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    // Handle the submission of the new meal
+                    addMeal(
+                      Meal(
+                        id: 'null',
+                        name: nameController.text,
+                        description: descriptionController.text,
+                        imageUrl:
+                            'https://firebasestorage.googleapis.com/v0/b/vida-meals.appspot.com/o/lasagna?alt=media&token=4fb7d92d-5b87-4dae-84b5-26dcb7868e32',
+                        price: double.parse(priceController.text),
+                      ),
+                    ).then((_) {
+                      Navigator.pop(context); // Close the modal
+                      onAddMeal(); // Trigger the callback to refresh data
+                    });
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
 }
 
 class AdminMenuPage extends StatefulWidget {
